00324ad3441bed0d368df3280d6e1bd9a52b80ae
franco <r.franco.s.facily@gmail.com>
2025-03-18 15:38:52 -0300

    update of the examples run individually as a component by index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,5 @@
-import { View } from 'react-native';
 import TelaCadastro from './telaCadastro';
-import ImageButton from './buttonImage';
-import ListaContatosEspecializada from './listaContatosEspecializada';
-import TelaValidCadastro from './telaValidCadastro';
 
 export default function Index() {
-  return <TelaValidCadastro />;
+  return <TelaCadastro />;
 }
